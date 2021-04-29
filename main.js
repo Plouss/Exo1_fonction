99b727248972b54c7c3919092def36518c5dc876
Plouss <80962741+Plouss@users.noreply.github.com>
2021-04-29 18:48:20 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,66 @@
+// //EXO1
+// function addition(a, b){
+//     console.log(a + b);
+// }
+// addition(2,2)
+
+// //EXO2
+// function soustraction(a, b){
+//     console.log(a - b);
+// }
+// soustraction(2,2)
+
+
+//EXO3
+// function mult(a, b){
+//     console.log(a * b);
+// }
+// mult(2,3)
+
+
+// //EXO4
+// function div(a, b){
+//     console.log(a / b);
+// }
+// div(4, 2)
+
+
+// // EXO5
+// function modulo(a, b){
+//     console.log(a % b);
+// }
+// modulo(10,4)
+
+
+// //EXO7
+// function exposant(nb12, nb13) {    
+//     return nb12**nb13;
+//     // return Math.pow(nb12, nb13);
+// }
+// console.log(exposant(8, 3));
+
+// Exo 8.
+function capitalize(phrase) {
+    return console.log(phrase.charAt(0).toUpperCase() + phrase.substr(1).toLowerCase());
+}
+capitalize("yo le Rap, yO lE RaP");
+
+// // Exo 9.
+// function calcul(n1, operator, n2) {
+//     switch (operator) {
+//         case '+':
+//             return addition(n1, n2);
+//         case '-':
+//             return n1 - n2;
+//         case '*':
+//             return n1 * n2;
+//         case '/':
+//             return n1 / n2;
+//         default:
+//             alert("Dors oslm")
+//             break;
+//     }
+// }
+// console.log(calcul(31, '+', 42));
+// return met fin à la function comme un break.
+// On peut appeler des fonctions dans d'autres fonctionnes comme avec les +.
